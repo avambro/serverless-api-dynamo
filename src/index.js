596292be41e50ee8be530c3eb415130e137ca990
Carlos Avalos <avambro@gmail.com>
2020-10-01 00:19:19 -0600

    insert values

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 const AWS = require('aws-sdk');
+const crypto = require("crypto");
+// Generate unique id with no external dependencies
+const generateUUID = () => crypto.randomBytes(16).toString("hex");
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
-const { v4: uuidv4 } = require("uuid");
 const processResponse = require('./process-response.js');
 const TABLE_NAME = process.env.TABLE_NAME,
   PRIMARY_KEY = process.env.PRIMARY_KEY,
   IS_CORS = true;
 
-exports.handler = async event => {
+exports.handler = async (event,context) => {
   if (event.httpMethod === 'OPTIONS') {
     return processResponse(IS_CORS);
   }
+  console.log("--request: " + JSON.stringify(event));
   if (!event.body) {
     return processResponse(IS_CORS, 'invalid', 400);
   }
-  const item = JSON.parse(event.body);
-  item[PRIMARY_KEY] = uuidv4();
+  const {body} = JSON.parse(event.body);
+
+  const item = {}
+  item[PRIMARY_KEY] = generateUUID();
+  item["title"] = body.title
+
+
+  //const UUID = context.awsRequestId;  
   const params = {
     TableName: TABLE_NAME,
     Item: item
